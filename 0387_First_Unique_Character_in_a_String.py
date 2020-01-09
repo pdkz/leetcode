ad709ae46b33afbe756d878633aaf390611fac1f
pdkz <pidkaz@gmail.com>
2020-01-09 23:19:54 +0900

    Update 387.

--- 0387_First_Unique_Character_in_a_String.py
+++ 0387_First_Unique_Character_in_a_String.py
@@ -1,22 +1,8 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        if len(s) == 0: return -1
-        if len(s) == 1: return 0
-        from collections import Counter
-        from collections import defaultdict
-        
-        wordcount = Counter()
-        wordindex = defaultdict(int)
-        
+        counts = collections.Counter(s)
+   
         for i, w in enumerate(s):
-            wordcount[w] += 1
-            if w not in wordindex: wordindex[w] = i
-        
-        words = [k for k,v in wordcount.items() if v == 1]
-        if len(words) == 0:
-            return -1
-        
-        counts = {k:v for k, v in wordindex.items() if k in words}
-        idx = min(counts.values())
-        
-        return idx
+            if counts[w] == 1:
+                return i
+        return -1
