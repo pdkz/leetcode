e174737c1495a92b5231bf8dca37ef1c9b86a476
pdkz <pidkaz@gmail.com>
2020-01-20 10:34:00 +0900

    Add 136. Single Number
    using extra memory

---
+++ 0136_Single Number.py
@@ -0,0 +1,5 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d = collections.Counter(nums)
+        keys = [k for k, v in d.items() if v == 1]
+        return keys[0]
