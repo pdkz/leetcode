5ed016c0daeb2e29d0e4cef9ccfbbb1d92888162
pdkz <pidkaz@gmail.com>
2020-01-25 17:39:38 +0900

    Updated 1002

--- 1002_Find_Common_Characters.py
+++ 1002_Find_Common_Characters.py
@@ -1,5 +1,26 @@
 class Solution:
     def commonChars(self, A: List[str]) -> List[str]:
+        """
+        a = [0 for i in range(26)]
+        b = [0 for i in range(26)]
+        
+        for word in A[0]:
+            for ch in word:
+                a[ord(ch) - ord('a')] += 1
+            
+        for word in A[1:]:
+            b = [0 for i in range(26)]
+            for ch in word:
+                b[ord(ch) - ord('a')] += 1
+            for i in range(26):
+                a[i] = min(a[i], b[i])
+        
+        result = []
+        for i in range(26):
+            for j in range(a[i]):
+                result.append(chr(ord('a') + i))
+        return result
+        """
         words = [collections.Counter(w) for w in A]
         l = len(words)
         result = []
