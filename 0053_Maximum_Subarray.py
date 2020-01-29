c57111a02d63a54fba1fe09692b9beb4a1b95b1a
pdkz <pidkaz@gmail.com>
2020-01-29 12:46:42 +0900

    Improved 53

--- 0053_Maximum_Subarray.py
+++ 0053_Maximum_Subarray.py
@@ -1,22 +1,17 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        begin = 0
-        end = begin + 1
-        
-        n = len(nums)
-        max_subarray = sum(nums[begin:end])
-        
-        while(begin < n and end < n+1):
-            subarray = nums[begin:end]
-            sum_subarray = sum(subarray)
+        l = len(nums)
+
+        max_subarray = nums[0]
+        sum_subarray = max_subarray
+
+        for i in range(1,l):
+            if sum_subarray < 0:
+                sum_subarray = nums[i]
+            else:
+                sum_subarray += nums[i]
 
             if sum_subarray > max_subarray:
                 max_subarray = sum_subarray
 
-            if sum_subarray < 0:
-                begin += 1
-                end = begin + 1
-            else:
-                end += 1
-                
         return max_subarray
