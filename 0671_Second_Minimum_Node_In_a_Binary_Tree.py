da9a03468fa5ef50db73ca3b1c4a2dfd562d206d
pdkz <pidkaz@gmail.com>
2020-02-13 12:06:51 +0900

    Add 671. Second Minimum Node In a Binary Tree

---
+++ 0671_Second_Minimum_Node_In_a_Binary_Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def __init__(self):
+        self.ht = collections.defaultdict(int)
+        
+    def findSecondMinimumValue(self, root: TreeNode) -> int:
+        self.traverse(root)
+        
+        vals = sorted(list(self.ht.keys()))
+        if len(vals) < 2:
+            return -1
+        
+        return vals[1]
+    
+    def traverse(self, node):
+        if node == None:
+            return
+        
+        self.ht[node.val] = 1
+        
+        self.traverse(node.left)
+        self.traverse(node.right)
