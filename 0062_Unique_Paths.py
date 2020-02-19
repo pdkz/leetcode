2d9ef1a8cf86b2468aaa47c1d9ec689bce972bf6
pdkz <pidkaz@gmail.com>
2020-02-19 13:54:33 +0900

    Add 62. Unique Paths

---
+++ 0062_Unique_Paths.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        board = [[0 for j in range(n)] for i in range(m)]
+        for i in range(n):
+            board[0][i] = 1
+
+        for i in range(1, m):
+            board[i][0] = 1
+            for j in range(1, n):
+                board[i][j] = board[i-1][j] + board[i][j-1]
+
+        return board[m-1][n-1]
