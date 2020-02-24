6c447caa98971888610251302c39609b9798f451
pdkz <pidkaz@gmail.com>
2020-02-24 16:15:33 +0900

    Improved 1
    O(n^2) to O(n)

--- 0001_Two_Sum.py
+++ 0001_Two_Sum.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for idx, i in enumerate(nums):
-            j = target - i
-            if j < 0:
-                continue
-            if j in nums and nums.index(j) > idx:
-                a = nums.index(i)
-                b = nums.index(j)
-                return [a, b]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ht = collections.defaultdict(int)
+        for i, n in enumerate(nums):
+            ht[n] = i
+            
+        for i, n in enumerate(nums):
+            diff = target - n
+            j = ht.get(diff)
+            if j and i != j:
+                return [i, j]
+        return None
