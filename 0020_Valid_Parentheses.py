e919c6ea87085898dc0941cc66623c9d7142647c
pdkz <pidkaz@gmail.com>
2020-07-05 11:18:27 +0900

    Updated 20

--- 0020_Valid_Parentheses.py
+++ 0020_Valid_Parentheses.py
@@ -5,5 +5,23 @@ class Solution:
         
         stack = []
         pairs = {'(':')', '{':'}', '[':']'}
+        
+        if s[0] not in pairs.keys():
+            return False
+        
         for bracket in s:
-            stack.append(bracket)
+            if bracket in pairs.keys():
+                stack.append(bracket)
+            else:
+                if len(stack) == 0:
+                    return False
+                l_bracket = stack.pop()
+                r_bracket = pairs[l_bracket]
+                
+                if r_bracket != bracket:
+                    return False        
+
+        if len(stack) > 0:
+            return False
+        
+        return True
