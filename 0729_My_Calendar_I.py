8a238487c7b44d3728ac65b1f39bcbc1f1a8ba81
pdkz <pidkaz@gmail.com>
2020-07-14 18:18:33 +0900

    Improved 729 O(nlogn) 1480ms->340ms

--- 0729_My_Calendar_I.py
+++ 0729_My_Calendar_I.py
@@ -1,15 +1,39 @@
+class Node:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    def insert(self, start, end):
+        if not self.root :
+            self.root = Node(start, end)
+            return True
+        return self._insert(self.root, start, end)
+
+    def _insert(self, node, start, end):
+        if end <= node.start:
+            if not node.left:
+                node.left = Node(start, end)
+                return True
+            else:
+                return self._insert(node.left, start, end)
+        elif start >= node.end:
+            if not node.right:
+                node.right = Node(start, end)
+                return True
+            else:
+                return self._insert(node.right, start, end)
+        return False
+
 class MyCalendar:
     def __init__(self):
-        self.cal = []
+        self.bst = BinarySearchTree()
 
     def book(self, start: int, end: int) -> bool:
-        if len(self.cal) == 0:
-            self.cal.append([start, end])
-            return True
-        
-        for s, e in self.cal:
-            if s < end and start < e:
-                return False
-                
-        self.cal.append([start, end])
-        return True
+        if not self.bst:
+            self.bst = BinarySearchTree()
+        return self.bst.insert(start, end)
