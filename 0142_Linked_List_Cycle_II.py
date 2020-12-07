1943dd80a58ed45e2922a36a72fb4128f3b82c38
pdkz <pidkaz@gmail.com>
2020-12-07 16:35:18 +0900

    Implemented Floyd's cycle detection algorithm

--- 0142_Linked_List_Cycle_II.py
+++ 0142_Linked_List_Cycle_II.py
@@ -3,18 +3,27 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
-        if head is None:
+        if not head:
             return None
         
-        nodes = set()
-        while(head is not None):
-            if head in nodes:
-                print(head.val)
-                return head
-            nodes.add(head)
-            head = head.next
+        slow = head
+        fast = head
+        while True:
+            if fast is None or fast.next is None:
+                return None
+
+            fast = fast.next.next
+            slow = slow.next
             
-        return None
+            if slow == fast:
+                break
+        
+        fast = head
+        while slow != fast:
+            if slow is None or fast is None:
+                return None
+            slow = slow.next
+            fast = fast.next
+        return slow
