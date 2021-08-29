c9a1ae4f303bf6c9de5bb4a2dad7543e2ac1a4f4
pdkz <pidkaz@gmail.com>
2021-08-29 15:27:24 +0900

    Added comments

--- 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique.py
+++ 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique.py
@@ -2,8 +2,12 @@ class Solution:
     def minDeletions(self, s: str) -> int:
         from collections import Counter 
         
+        # 出現頻度数を降順ソート
         freqs = sorted(Counter(s).values(), reverse=True)
         max_frequency = max(freqs)
+
+        # 最大の合計出現頻度数
+        # 最大出現頻度が4ならば合計出現頻度数は4+3+2+1=12
         total_frequency = 0
         for i in range(0, max_frequency+1):
             total_frequency += i
@@ -12,6 +16,7 @@ class Solution:
         delete_count = 0
         while len(freqs) > 0:
             if len(unique_freqs) >= max_frequency:
+                # 全ての出現頻度が出揃った
                 break
 
             freq = freqs.pop(0)
@@ -21,11 +26,13 @@ class Solution:
                     unique_freqs.append(freq)
                     break
                 else:
+                    # すでに選択されているので,頻度を減らしていく
                     freq -= 1
                     delete_count += 1
                     total_frequency -= 1
 
         if len(freqs) > 0:
+            # 選択できなかった文字を全て削除
             delete_count += sum(freqs)
         
         return delete_count
