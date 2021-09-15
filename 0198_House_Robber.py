b544e77f7ae289540c3d8e28bb3eca3469b8f1e2
pdkz <pidkaz@gmail.com>
2021-09-15 23:38:34 +0900

    Updated 198.

--- 0198_House_Robber.py
+++ 0198_House_Robber.py
@@ -1,26 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        N = len(nums)
-        if N == 1:
-            return nums[0]
-        elif N == 2:
+        if len(nums) < 3:
             return max(nums)
 
-        dp = [0] * (N+1)
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
 
-        dp[1] = nums[0]
-        dp[2] = nums[1]
-
-        total_first = dp[1]
-        total_second = dp[2]
-
-        i = 3
-        for n in nums[2:]:
-            if i % 2:
-                total_first += n
-                dp[i] = max(dp[i-1], dp[i-2]+n, dp[i-3]+n, total_first)
-            else:
-                total_second += n
-                dp[i] = max(dp[i-1], dp[i-2]+n, dp[i-3]+n, total_second)
-            i += 1
-        return dp[N]
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        return dp[-1]
