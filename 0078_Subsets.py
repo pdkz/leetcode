3bc49713b01026b08f6a5863f59d9b9169a2c91e
pdkz <pidkaz@gmail.com>
2021-11-06 20:30:13 +0900

    Updated 78

--- 0078_Subsets.py
+++ 0078_Subsets.py
@@ -1,21 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
-        subset = []
-        l = len(nums)
-        
-        subset.append([])
-        
-        for i in nums:
-            subset.append([i])
-        
-        for n in range(0,l-1):
-            for i in subset:
-                if len(i) != n+1: 
-                    continue
-                offset = nums.index(max(i))
-                
-                for j in nums[offset+1:]:
-                    subset.append(i+[j])
-        
-        return subset
+        def dfs(nums, res, tmp, start):
+            res.append(tmp[:])
+
+            for i in range(start, len(nums)):
+                tmp.append(nums[i])
+                dfs(nums, res, tmp, i+1)
+                tmp.pop()
+
+        res = []
+        dfs(nums, res, [], 0)
+
+        return res
