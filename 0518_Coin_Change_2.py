6159542b71a21b315222db5688512af0d22eb665
pdkz <pidkaz@gmail.com>
2022-03-12 15:20:52 +0900

    Removed redunduncies

--- 0518_Coin_Change_2.py
+++ 0518_Coin_Change_2.py
@@ -1,17 +1,16 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        N = len(coins)
         dp = []
-        for i in range(N):
+        N = len(coins)
+        for i in range(N+1):
             dp.append([0 for _ in range(amount+1)])
-
-        for i in range(N):
+        for i in range(N+1):
             dp[i][0] = 1
-            for j in range(amount+1):
-                if coins[i] <= j:
-                    if i > 0: dp[i][j] = dp[i][j-coins[i]] + dp[i-1][j]
-                    else:
-                        dp[i][j] = dp[i][j-coins[i]]
-                elif i > 0:
+
+        for i in range(1,N+1):
+            for j in range(1,amount+1):
+                if coins[i-1] <= j:
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j]
+                else:
                     dp[i][j] = dp[i-1][j]
         return dp[-1][-1]
