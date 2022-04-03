e7fc5ec8059fa6559a3a122db7b9512b26689314
pdkz <pidkaz@gmail.com>
2022-04-03 18:43:40 +0900

    Updated 3.

--- 0003_Longest_Substring_Without_Repeating_Characters.py
+++ 0003_Longest_Substring_Without_Repeating_Characters.py
@@ -1,35 +1,12 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 0:
-            return 0
+        hashmap = set()
+        l, ans, N = 0, 0, len(s)
 
-        table = collections.defaultdict(int)
-        nlen = len(s)
-        substr = s[0]
-        max_substr_length = len(substr)
-        table[substr] = 0
-
-        begin = 0
-        end = 0
-
-        while begin <= end and begin < nlen and end < nlen:
-            v = table.get(s[end])
-            if begin < end:
-                if v != None and v >= 0:
-                    for i in range(begin, v+1):
-                        del table[s[i]]
-
-                    begin = v + 1
-                    substr = s[begin:end+1]
-                    
-                    table[s[begin]] = begin
-                    table[s[end]] = end
-                else:
-                    substr += s[end]
-                    table[s[end]] = end
-            end += 1
-
-            if len(substr) > max_substr_length:
-                max_substr_length = len(substr)
-
-        return max_substr_length
+        for r in range(N):
+            while s[r] in hashmap:
+                hashmap.discard(s[l])
+                l += 1
+            hashmap.add(s[r])
+            ans = max(ans, r-l+1)
+        return ans
