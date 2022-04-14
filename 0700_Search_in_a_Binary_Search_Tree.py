aabc7f2e68d2fef35f4f36e289611f7330182a93
pdkz <pidkaz@gmail.com>
2022-04-14 10:31:59 +0900

    Added a dfs solution

--- 0700_Search_in_a_Binary_Search_Tree.py
+++ 0700_Search_in_a_Binary_Search_Tree.py
@@ -6,7 +6,10 @@
 #         self.right = None
 
 class Solution:
-    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+    def searchBST(self, root: TreeNode, val: int)  -> Optional[TreeNode]:
+        return self.bfs(root, val)
+
+    def bfs(self, root: TreeNode, val: int) -> Optional[TreeNode]:
         queue = [root]
         while len(queue) > 0:
             node = queue.pop(0)
@@ -19,3 +22,17 @@ class Solution:
             if r:
                 queue.append(r)
         return None
+
+    def dfs(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        def helper(node, val):
+            if not node:
+                return None
+            if node.val == val:
+                return node
+
+            lnode = dfs(node.left, val)
+            rnode = dfs(node.right, val)
+
+            return lnode if lnode else rnode
+
+        return helper(root, val)
