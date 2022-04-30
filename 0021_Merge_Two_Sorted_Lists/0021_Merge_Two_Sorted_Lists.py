d438f9836cb2488c8b73a935df64c7c9922a50b4
pdkz <pidkaz@gmail.com>
2022-04-30 12:39:58 +0900

    Added recursive solution in Python

--- 0021_Merge_Two_Sorted_Lists/0021_Merge_Two_Sorted_Lists.py
+++ 0021_Merge_Two_Sorted_Lists/0021_Merge_Two_Sorted_Lists.py
@@ -23,3 +23,16 @@ class Solution:
             tail.next = list2
 
         return dummy.next
+
+    def mergeTwoListsRecursively(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoListsRecursively(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoListsRecursively(list1, list2.next)
+            return list2
