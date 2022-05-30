aad6950ae827f6a0552b18ca19c8309f96786317
pdkz <pidkaz@gmail.com>
2022-05-30 09:27:13 +0900

    Updated 20./

--- 0020_Valid_Parentheses/0020_Valid_Parentheses.py
+++ 0020_Valid_Parentheses/0020_Valid_Parentheses.py
@@ -1,27 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) == 0:
-            return True
-        
         stack = []
-        pairs = {'(':')', '{':'}', '[':']'}
-        
-        if s[0] not in pairs.keys():
-            return False
-        
-        for bracket in s:
-            if bracket in pairs.keys():
-                stack.append(bracket)
-            else:
-                if len(stack) == 0:
-                    return False
-                l_bracket = stack.pop()
-                r_bracket = pairs[l_bracket]
-                
-                if r_bracket != bracket:
-                    return False        
+        pairs = { ')':'(', '}':'{', ']':'['}
 
-        if len(stack) > 0:
-            return False
-        
-        return True
+        for c in s:
+            if c in pairs.values():
+                stack.append(c)
+            else:
+                if stack and pairs[c] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+        return True if not stack else False
