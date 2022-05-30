27ab3dca32dae81d8e11aab174e878bb810a786c
pdkz <pidkaz@gmail.com>
2022-05-30 09:27:33 +0900

    Updated 1539.

--- 1539_Kth_Missing_Positive_Number/1539_Kth_Missing_Positive_Number.py
+++ 1539_Kth_Missing_Positive_Number/1539_Kth_Missing_Positive_Number.py
@@ -1,33 +1,13 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        missing_count = 0
-        prev_idx = prev_n = 0
-        ans = 0
+        def count_missing_numbers(arr, i):
+            return arr[i] - i - 1
 
-        for i, n in enumerate(arr):
-            missing_count = n - i - 1
-            if prev_idx <= k and k <= missing_count:
-                idx = self.binary_search(prev_idx+1, missing_count, k)
-                ans = prev_n + idx + 1
-                break
-
-            prev_idx = missing_count
-            prev_n = n
-
-        if ans == 0:
-            ans = arr[-1] + k - missing_count
-        return ans
-
-    def binary_search(self, i, j, k):
-        arr = [_ for _ in range(i, j+1)]
-        begin = 0
-        end = len(arr)-1
-        while begin <= end:
-            m = (begin+end) // 2
-            if arr[m] < k:
-                begin = m + 1
-            elif arr[m] > k:
-                end = m - 1
+        lo, hi = 0, len(arr)
+        while lo < hi:
+            mid = lo + (hi-lo) // 2
+            if count_missing_numbers(arr, mid) < k:
+                lo = mid + 1
             else:
-                break
-        return m
+                hi = mid
+        return arr[lo-1]+k-count_missing_numbers(arr, lo-1)
