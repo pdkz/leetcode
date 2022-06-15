9b1404d7ad6e7228b08cd95c9d008b15370c11ec
pdkz <pidkaz@gmail.com>
2022-06-15 22:29:34 +0900

    Updated 268 solved using binary search

--- 0268_Missing_Number/0268_Missing_Number.py
+++ 0268_Missing_Number/0268_Missing_Number.py
@@ -5,3 +5,15 @@ class Solution:
             if i != j:
                 return i
         return n[-1] + 1
+
+    def missingNumber_binary_search(self, nums: List[int]) -> int:
+        nums.sort()
+        ok, ng = -1, len(nums)
+
+        while abs(ok-ng) > 1:
+            mid = ok+(ng-ok) // 2
+            if nums[mid] <= mid:
+                ok = mid
+            else:
+                ng = mid
+        return nums[ok]+1 if ng == len(nums) else nums[ng]-1
