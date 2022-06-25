12ab570d036229d1b3a66b6b7980780d747c2f3e
pdkz <pidkaz@gmail.com>
2022-06-25 10:34:04 +0900

    Updated 199.

--- 0199_Binary_Tree_Right_Side_View/0199_Binary_Tree_Right_Side_View.py
+++ 0199_Binary_Tree_Right_Side_View/0199_Binary_Tree_Right_Side_View.py
@@ -7,14 +7,32 @@ class Solution:
             height, node = queue.pop(0)
             if not node:
                 break
-            
+
             if height == current + 1:
                 current = height
                 rsv.append(node.val)
-            
+
             if node.right:
                 queue.append((current+1, node.right))
             if node.left:
                 queue.append((current+1, node.left))
-    
+
         return rsv
+
+    def rightSideView_level_order_traversal(self, root: TreeNode) -> List[int]:
+        res = []
+        q = deque([root])
+
+        while q:
+            right_side = None
+            q_len = len(q)
+
+            for i in range(q_len):
+                node = q.popleft()
+                if node:
+                    right_side = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if right_side:
+                res.append(right_side.val)
+        return res
