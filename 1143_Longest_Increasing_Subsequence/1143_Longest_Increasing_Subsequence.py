f3d6ed1fd7f52b2a00e8e8aeb5132dd6328b216c
pdkz <pidkaz@gmail.com>
2022-07-06 20:30:12 +0900

    Added 1143. Longest Increasing Subsequence

---
+++ 1143_Longest_Increasing_Subsequence/1143_Longest_Increasing_Subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        M, N = len(text1), len(text2)
+        dp = [[0 for _ in range(M+1)] for _ in range(N+1)]
+
+        for i in range(N):
+            for j in range(M):
+                if text2[i] == text1[j]:
+                    dp[i+1][j+1] = dp[i][j] + 1
+                else:
+                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+        return dp[-1][-1]
