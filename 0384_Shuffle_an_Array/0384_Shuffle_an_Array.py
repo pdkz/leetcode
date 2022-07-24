b5a97db8a23c2e93a9a8ebec3639c18d65822490
pdkz <pidkaz@gmail.com>
2022-07-24 22:37:57 +0900

    Added 384. Shuffle an Array

---
+++ 0384_Shuffle_an_Array/0384_Shuffle_an_Array.py
@@ -0,0 +1,24 @@
+import random
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.orig = defaultdict()
+        for i, n in enumerate(nums):
+            self.orig[i] = n
+
+    def reset(self) -> List[int]:
+        for i, n in self.orig.items():
+            self.nums[i] = n
+        return self.nums
+
+    def shuffle(self) -> List[int]:
+        random.shuffle(self.nums)
+        return self.nums
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
